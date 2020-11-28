bb2f5687c30fb1a905e735a322444bfd7cf06da2
nicholaskraabel <67570600+nicholaskraabel@users.noreply.github.com>
2020-11-28 15:00:06 -0800

    CardTexture
    s

--- PA8_SFML/main.cpp
+++ PA8_SFML/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "Card.h"
 
 int main()
 {
@@ -6,16 +7,17 @@ int main()
     //main game loop
     while(window.isOpen())
     {
-        sf::RectangleShape RS;
-        RS.setFillColor(sf::Color::Red);
-        RS.setPosition(500, 500);
+        sf::RectangleShape RS(sf::Vector2f(100.f, 100.f));
+        sf::Texture t;
+        Card club1 = Card(RS, 1, 'c', "CLUB-1.jpg");
+        
        
       //Stages for go Fish
         //start of game intailizeing deck shuffle dealing 
         //ends when deck runs out
 
         window.clear();
-        window.draw(RS);
+        club1.draw(window, 550, 550);
         window.display();
     }
 
