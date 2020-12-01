16b7dc8d5356330338540ced04966357e20d4a3e
danielclawson <74019469+danielclawson@users.noreply.github.com>
2020-11-30 17:43:10 -0800

    Revert "Added menu"
    This reverts commit 76cb36321e95764e66ef58fddedc493f869c89e5.

--- PA8_SFML/DeckConstruction.h
+++ PA8_SFML/DeckConstruction.h
@@ -4,10 +4,6 @@
 
 //This is just called once and creates all the cards for the game. 
 void deckConstuctor(Card(&deck)[deckSize]);
-<<<<<<< Updated upstream
 
 //This is the actual stack used for the game which is given.
 void gameDeck(std::stack <Card>&);
-=======
- 
->>>>>>> Stashed changes
--- PA8_SFML/main.cpp
+++ PA8_SFML/main.cpp
@@ -1,28 +1,15 @@
-//#include <SFML/Graphics.hpp>
-//#include "Card.h"
-//#include "DeckConstruction.h"
-//#include <Windows.h>
-
-#include "Header.h"
+#include <SFML/Graphics.hpp>
+#include "Card.h"
+#include "DeckConstruction.h"
+#include <Windows.h>
 
 int main()
 {
-    int input = 0;
-    //sf::RenderWindow window(sf::VideoMode(1000, 1000), "GO Fish");
-
-    // for the board
-    sf::RenderWindow window(sf::VideoMode(1700, 950), "Go Fish"); // , sf::Style::Fullscreen);
-   
-
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "GO Fish");
     //main game loop
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+
 
         //Stages for go Fish
           //start of game intailizeing deck shuffle dealing 
@@ -30,17 +17,14 @@ int main()
         Card deck[deckSize]; 
         deckConstuctor(deck); 
         window.clear();
-        /*for (int i = 0; i < 52; i++)
+        for (int i = 0; i < 52; i++)
         {
             deck[i].draw(window, 80 + (i * 15), 500);
-        }*/
-        //deck[0].drawBack(window, 450, 150);
-        draw_window(window); // displays the main screen
-        input = menu(window);
-        window.display(); 
-
-        //Sleep(100000);
-        //window.close();
+        }
+        deck[0].drawBack(window, 450, 150);
+        window.display();
+        Sleep(100000);
+        window.close();
     }
 
     return 0;
